112d2a929b27076d700cbe62f53242a2cbe2238b
sociopathsourya <souryamitra95@gmail.com>
2017-06-04 23:45:01 +0530

    Insert

---
+++ src/TestClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: morningstar
+ * Date: 4/6/17
+ * Time: 11:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+}
